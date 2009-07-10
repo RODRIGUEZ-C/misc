8170493cb3fc935cdbd7b61adaf15abffa861326
mokehehe <mokehehe@gmail.com>
2009-07-10 22:16:27 +0900

    add array, can use struct

--- ruby/packedstruct.rb
+++ ruby/packedstruct.rb
@@ -1,228 +1,200 @@
-# $Id: dbf.rb,v 1.9 2006/02/12 22:02:54 aamine Exp $
-#
-# Copyright (c) 2005,2006 yrock
-# 
-# This program is free software.
-# You can distribute/modify this program under the terms of the Ruby License.
-#
-# 2006-02-11 refactored by Minero Aoki
+class PackedStruct
+  class << PackedStruct
+    def define(&block)
+      c = Class.new(self)
+      def c.inherited(subclass)
+        proto = @prototypes
+        subclass.instance_eval {
+          @prototypes = proto
+        }
+      end
+      c.module_eval(&block)
+      c
+    end
 
-# 2008 modified by mokehehe
-#		add default value
+    def string(n, name)
+      define_field name, "Z#{n}", n
+    end
 
-  class PackedStruct
-    class << PackedStruct
-      def define(&block)
-        c = Class.new(self)
-        def c.inherited(subclass)
-          proto = @prototypes
-          subclass.instance_eval {
-            @prototypes = proto
-          }
+    def array(type, name, num)
+      case type
+      when Symbol, String
+        if @@types.has_key?(type.to_sym)
+          e = @@types[type.to_sym]
+          type = FieldPrototype.new(:dmy, e[0], e[1])
+        else
+          raise "type not defined: #{type}"
         end
-        c.module_eval(&block)
-        c
       end
+      klass = FieldArray.new(type, num)
 
-      def char(name, val=?\00)
-        define_field name, 'A', 1, val
-      end
+      (@prototypes ||= []).push klass
+      (@names ||= []).push name
+      define_accessor name
+    end
 
-      def byte(name, val=0)
-        define_field name, 'C', 1, val
-      end
+    def serialize(v)
+      v.serialize
+    end
 
-      def int16LE(name, val=0)
-        define_field name, 'v', 2, val
-      end
-
-      def int32LE(name, val=0)
-        define_field name, 'V', 4, val
-      end
-
-      def string(n, name, val="")
-        define_field name, "Z#{n}", n, val
-      end
-
-
-      # 追加の型
-      def int(name, val=0)
-        int32LE(name, val)
-      end
-
-      def short(name, val=0)
-        int16LE(name, val)
-      end
-
-      def UINT(name, val=0)
-        int32LE(name, val)
-      end
-
-      def pointer(name, val=nil)
-        define_field name, "P", 4, val
-      end
-
-      def LPSTR(name, val=nil)
-        pointer(name, val)
-      end
-
-      def LPARAM(name, val=nil)
-        int32LE(name, val)
-      end
-
-      def PUINT(name, val=nil)
-        pointer(name, val)
-      end
-
-      def WORD(name, val=nil)
-        int16LE(name, val)
-      end
-
-      def DWORD(name, val=nil)
-        int32LE(name, val)
-      end
-
-      def LONG(name, val=nil)
-        int32LE(name, val)
-      end
+    def define_type(typename, template_string, bytesize)
+      @@types[typename.to_sym] = [template_string, bytesize]
+    end
 
     private
 
-      def define_field(name, template, size, default_value)
-        (@prototypes ||= []).push FieldPrototype.new(name, template, size, default_value)
-        define_accessor name
-      end
+    @@types = {
+      :char    => ['A', 1],
+      :byte    => ['C', 1],
+      :int16LE => ['v', 2],
+      :int32LE => ['V', 4],
+    }
 
-      def define_accessor(name)
-        module_eval(<<-End, __FILE__, __LINE__ + 1)
-          def #{name}
-            self['#{name}']
-          end
-
-          def #{name}=(val)
-            self['#{name}'] = val
-          end
-        End
-      end
+    def define_field(name, template, size)
+      (@prototypes ||= []).push FieldPrototype.new(name, template, size)
+      (@names ||= []).push name
+      define_accessor name
     end
 
-    class FieldPrototype
-      def initialize(name, template, size, default_value)
-        @name = name
-        @template = template
-        @size = size
-        @default_value = default_value
-      end
-
-      attr_reader :name
-      attr_reader :size
-      attr_reader :default_value
-
-      def read(f)
-        parse(f.read(@size))
-      end
-
-      def parse(s)
-        s.unpack(@template)[0]
-      end
-
-      def serialize(val)
-        tmpl = @template
-        case tmpl
-        when 'v', 'V'
-			val = 0 if val == nil
+    def define_accessor(name)
+      module_eval(<<-End, __FILE__, __LINE__ + 1)
+        def #{name}
+          self['#{name}']
         end
-        [val].pack(tmpl)
+
+        def #{name}=(val)
+          self['#{name}'] = val
+        end
+      End
+    end
+
+    def method_missing(type, name, *args)
+      if @@types.has_key?(type)
+        if name.kind_of?(Array)
+          array(type, name[0], name[1])
+        else
+          e = @@types[type]
+          define_field name, e[0], e[1]
+        end
+      else
+        # 文字列からグローバルに定義されてるクラスを探したい
+        begin
+          klass = eval(type.to_s)
+        rescue
+          raise "no type: #{type}"
+        end
+        if klass.kind_of?(Class) && klass.new.kind_of?(PackedStruct)
+          if name.kind_of?(Array)
+            array(klass, name[0], name[1])
+          else
+            (@prototypes ||= []).push klass
+            (@names ||= []).push name
+            define_accessor name
+          end
+        else
+          raise "not type name: #{type}"
+        end
       end
-    end
-
-    def PackedStruct.size
-      @prototypes.map {|proto| proto.size }.inject(0) {|sum, s| sum + s }
-    end
-
-    def PackedStruct.names
-      @prototypes.map {|proto| proto.name }
-    end
-
-    def PackedStruct.prototypes
-      @prototypes
-    end
-
-    def PackedStruct.default_values
-      @prototypes.map {|proto| proto.default_value }
-    end
-
-    def PackedStruct.read(f)
-      new(* @prototypes.map {|proto| proto.read(f) })
-    end
-
-    def initialize(*vals)
-      v = vals
-      arg_num = vals.size
-      n = self.class.prototypes.size
-      if arg_num < n
-        v = v + self.class.default_values[arg_num, n - arg_num]
-      end
-      @alist = self.class.names.zip(v)
-    end
-
-    def inspect
-      "\#<#{self.class} #{@alist.map {|n,v| "#{n}=#{v.inspect}" }.join(' ')}>"
-    end
-
-    def [](name)
-      k, v = @alist.assoc(name.to_s.intern)
-      raise ArgumentError, "no such field: #{name}" unless k
-      v
-    end
-
-    def []=(name, val)
-      a = @alist.assoc(name.to_s.intern)
-      raise ArgumentError, "no such field: #{name}" unless a
-      a[1] = val
-    end
-
-    def serialize
-      self.class.prototypes.zip(@alist.map {|_, val| val })\
-          .map {|proto, val| proto.serialize(val) }.join('')
-    end
-
-
-
-
-    def pack
-      serialize
-    end
-
-    def size
-      self.class.size
-    end
-
-
-
-	class StringStream
-		def initialize(str)
-			@str = str
-			@idx = 0
-		end
-	
-		def read(size)
-			unless eof?
-				idx = @idx
-				@idx += size
-				@str[idx, size]
-			else
-				""
-			end
-		end
-	
-		def eof?
-			@idx >= @str.length
-		end
-	end
-
-    def PackedStruct.unpack(str)
-      f = StringStream.new(str)
-      read(f)
     end
   end
+
+  class FieldPrototype
+    def initialize(name, template, size)
+      @name = name
+      @template = template
+      @size = size
+    end
+
+    attr_reader :name
+    attr_reader :size
+
+    def read(f)
+      parse(f.read(@size))
+    end
+
+    def parse(s)
+      s.unpack(@template)[0]
+    end
+
+    def serialize(val)
+      [val].pack(@template)
+    end
+  end
+
+  class FieldArray
+    def initialize(type, num)
+      @type = type
+      @num = num
+      @size = type.size * num
+    end
+
+    attr_reader :size
+
+    def read(f)
+      parse(f.read(@size))
+    end
+
+    def parse(s)
+      elemsize = @type.size
+      (0...@num).map do |i|
+        @type.parse(s[i * elemsize, elemsize])
+      end
+    end
+
+    def serialize(vals)
+       vals.map {|val| @type.serialize(val)}.join('')
+    end
+  end
+
+  def PackedStruct.size
+    @prototypes.map {|proto| proto.size }.inject(0) {|sum, s| sum + s }
+  end
+
+  def PackedStruct.names
+    @names
+  end
+
+  def PackedStruct.prototypes
+    @prototypes
+  end
+
+  def PackedStruct.read(f)
+    new(* @prototypes.map {|proto| proto.read(f) })
+  end
+
+  def initialize(*vals)
+    @alist = self.class.names.zip(vals)
+  end
+
+  def inspect
+    "\#<#{self.class} #{@alist.map {|n,v| "#{n}=#{v.inspect}" }.join(' ')}>"
+  end
+
+  def [](name)
+    k, v = @alist.assoc(name.to_s.intern)
+    raise ArgumentError, "no such field: #{name}" unless k
+    v
+  end
+
+  def []=(name, val)
+    a = @alist.assoc(name.to_s.intern)
+    raise ArgumentError, "no such field: #{name}" unless a
+    a[1] = val
+  end
+
+  def serialize
+    self.class.prototypes.zip(@alist.map {|_, val| val })\
+        .map {|proto, val| proto.serialize(val) }.join('')
+  end
+end
+
+
+if $0 == __FILE__
+	Test = PackedStruct.define {
+		int32LE :hoge
+		string 8, :fuga
+	}
+	test = Test.new(0x12345678, "Ruby")
+	p test
+	p test.serialize
+end
